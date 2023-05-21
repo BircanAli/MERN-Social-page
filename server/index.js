83eea28b1487ac0b38d9108c9f888c4c901ea145
Birdzhan Ali <105841521+BircanAli@users.noreply.github.com>
2023-05-21 18:55:19 +0200

    new commit

--- server/index.js
+++ server/index.js
@@ -66,8 +66,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     // ADD DATA ONE IME
-    // User.insertMany(users)
-    // Post.insertMany(posts)
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
 
   .catch((error) => console.log(`${error} did not connect`));
